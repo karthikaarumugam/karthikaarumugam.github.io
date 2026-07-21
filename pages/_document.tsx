d31672dfce263712aca08bcbb1995a0a3ed64793
Karthika Arumugam <karthika_a2006@live.com>
2026-07-20 21:04:54 -0500

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,14 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Karthika Arumugam",
-    description: "Portfolio of Karthika Arumugam, Tech Lead",
+    description: "Portfolio of Karthika Arumugam, Engineering Leader",
 };
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <meta name="description" content="Portfolio of Karthika Arumugam, Tech Lead" />
+                <meta name="description" content="Portfolio of Karthika Arumugam, Engineering Leader" />
             </Head>
             <body>
                 <Main />
